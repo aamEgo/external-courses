9e9fecff692fb19d539c32aa7545dd53729ddf29
aam1go <aam1go>
2019-03-25 01:21:15 +0300

    5.1 fix this,self

--- task5/mission1.js
+++ task5/mission1.js
@@ -1,58 +1,57 @@
-var calculator = {
-    result: 0,
-    add: function (val) {
-        val = typeof val == 'number' ? val : 0;
-        var self = this;
-        var f = function (arg) {
-            self.result += arg;
-            return f;
-        }
-        return f(val);
-    },
-    subtract: function (val) {
-        val = typeof val == 'number' ? val : 0;
-        var self = this;
-        var f = function (arg) {
-            self.result -= arg;
-            return f;
-        }
-        return f(val);
-    },
-    divide: function (val) {
-        val = typeof val == 'number' ? val : 0;
-        var self = this;
-        var f = function (arg) {
-            self.result /= arg;
-            return f;
-        }
-        return f(val);
-    },
-    multiply: function (val) {
-        val = typeof val == 'number' ? val : 0;
-        var self = this;
-        var f = function (arg) {
-            self.result *= arg;
-            return f;
-        }
-        return f(val);
-    },
+function Calculator() {
+    var result = 0;
 
+    function add(val) {
+        val = typeof val == 'number' ? val : 0;
+        result += val;
+        return add;
+    }
 
-    getResult: function () {
-        return this.result;
-    },
-    reset: function () {
-        this.result = 0;
-    },
+    function subtract(val) {
+        val = typeof val == 'number' ? val : 0;
+        result -= val;
+        return subtract;
+    }
+
+    function divide(val) {
+        val = typeof val == 'number' ? val : 0;
+        result += val;
+        return divide;
+    }
+
+    function multiply(val) {
+        val = typeof val == 'number' ? val : 0;
+        result += val;
+        return multiply;
+    }
+
+    function getResult() {
+        return result;
+    }
+
+    function reset() {
+        result = 0;
+    }
+
+    return {
+        add: add,
+        subtract: subtract,
+        divide: divide,
+        multiply: multiply,
+
+        getResult: getResult,
+        reset: reset,
+    }
 
 }
 
 function test() {
-    calculator.add(0)(3)(4)(10);//17
+    var calculator = Calculator();
+    calculator.add('a')(3)(4)(10);//17
     console.info(calculator.getResult());
     calculator.subtract(3);//14
     console.info(calculator.getResult());
-    calculator.divide(7)(1);//2
+    calculator.divide(7)(1);//22
     console.info(calculator.getResult());
     calculator.multiply(6)(14);
     console.info(calculator.getResult());
